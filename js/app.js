c4de3b02485a4049a1cae8fc668f1985f7792137
lenonbr <lenon.br@hotmail.com>
2025-03-27 15:21:50 -0300

    criando variáveis

---
+++ js/app.js
@@ -0,0 +1,12 @@
+function limpar(id, valor, quantidade) {
+
+}
+
+function adicionar(id, valor, quantidade) {
+
+}
+let produto = [
+    {id: 1, nome:'Fone de ouvido', valor: 100, quantidade: 0},
+    {id: 2, nome: 'Celular', valor: 1400, quantidade: 0},
+    {id: 3, nome:'Oculus VR', valor: 5000, quantidade: 0} 
+]
