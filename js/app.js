8ebfb249c1497a9e65db9d234858ee7ffb68c48f
lenonbr <lenon.br@hotmail.com>
2025-03-27 17:59:25 -0300

    carrinho finalizado

--- js/app.js
+++ js/app.js
@@ -1,19 +1,28 @@
+let totalGeral;
+limpar();
+
 function adicionar() {
+    //recuperar valores nome do produto, quantidade e valor
     let produto = document.getElementById('produto').value;
-    let quantidade = document.getElementById('quantidade').value;
     let nomeProduto = produto.split('-')[0];
-    let valorProduto = produto.split('R$')[1];
-    let preco = valorProduto * quantidade;
-
-
+    let valorUnitario = produto.split('R$')[1];
+    let quantidade = document.getElementById('quantidade').value; 
+    //calcular o preço, o nosso subtotal
+    let preco = quantidade * valorUnitario;
     let carrinho = document.getElementById('lista-produtos');
+    //adicionar no carrinho
     carrinho.innerHTML = carrinho.innerHTML + `<section class="carrinho__produtos__produto">
-          <span class="texto-azul">${quantidade}x</span> ${nomeProduto} <span class="texto-azul">R$${preco}</span>
-        </section>`
-
+    <span class="texto-azul">${quantidade}x</span> ${nomeProduto} <span class="texto-azul">R${preco}</span>
+  </section>`;
+    //atualizar o valor total
+    totalGeral = totalGeral + preco;
+    let campoTotal = document.getElementById('valor-total'); 
+    campoTotal.textContent =  `R$ ${totalGeral}`;
+    document.get.ElementById('quantidade').value = 0;
 }
 
-function limpar(carrinho) {
-    carrinho = '';
+function limpar() {
+    totalGeral = 0;
+    document.getElementById('lista-produtos').innerHTML = '';
+    document.getElementById('valor-total').textContent = 'R$ 0';
 }
-
